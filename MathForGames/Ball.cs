b1e134fb2f65f558dd1b7da869d6345ac37b6e7b
Brittnie <brittniesexton95@gmail.com>
2021-10-13 13:03:48 -0500

    Added ball class

---
+++ MathForGames/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary;
+
+namespace MathForGames
+{
+    class Ball
+    {
+        private Vector2 _velocity;
+
+        public Vector2 Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public override void OnCollision(Actor actor)
+        {
+
+        }
+        
+
+    }
+}
